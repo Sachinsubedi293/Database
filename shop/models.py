a1eb0f801b3bb8952918f81d693d18b7b294b0ff
Sachinsubedi293 <sachinsubedi616@gmail.com>
2021-10-20 14:19:58 +0545

    'second'

--- shop/models.py
+++ shop/models.py
@@ -18,10 +18,14 @@ class category(models.Model):
 class Product(models.Model):
     category = models.ForeignKey(category, related_name='products', on_delete=models.CASCADE)
     name = models.CharField(max_length=200, db_index=True)
-
-    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True,null=True)
+    image = models.ImageField(upload_to='products/%Y/%m/', blank=True,null=True)
+    extraimage1 = models.ImageField(upload_to='products/%Y/%m', blank=True, null=True)
+    extraimage2 = models.ImageField(upload_to='products/%Y/%m', blank=True, null=True)
+    extraimage3 = models.ImageField(upload_to='products/%Y/%m', blank=True, null=True)
     description = models.TextField(blank=True)
-    price = models.IntegerField()
+    price = models.IntegerField(default=0)
+    discount = models.IntegerField(default=0)
+    sale = models.BooleanField(default=False)
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
